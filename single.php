101ec1ff2cdeafc59b6b452ee861c988f0cb681e
Mike Sewell <mike@maikunari.com>
2013-03-15 14:34:47 -0400

    Removed sharing icons
    Removed sharing icons as Jetpack & other plugins have this covered.

--- single.php
+++ single.php
@@ -24,19 +24,6 @@
 						
 						<?php the_content(); ?>
 						
-						<div class="social-links-single">
-							<div class="twitter-single">
-								<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="mikesensei">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-							</div><!-- .twitter-single -->
-							
-							<div class="facebook-single">
-								<iframe src="http://www.facebook.com/plugins/like.php?href=<?php echo urlencode(get_permalink($post->ID)); ?>&amp;layout=standard&amp;show_faces=false&amp;
-								width=450&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:450px; height:30px;">
-								</iframe>
-							</div><!-- .facebook-single -->
-							
-						</div><!-- .social-links-single -->
-						
 						
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sutra' ), 'after' => '</div>' ) ); ?>
 					</div><!-- .entry-content -->
